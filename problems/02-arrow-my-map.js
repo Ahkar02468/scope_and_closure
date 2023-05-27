95b94ea658e64df6df274a1eecd00ed2559611f4
Ahkar02468 <arkarshwebaw@gmail.com>
2023-05-27 15:08:41 +0630

    solve 02-arrow-my-map

--- problems/02-arrow-my-map.js
+++ problems/02-arrow-my-map.js
@@ -1,7 +1,7 @@
 /***********************************************************************
 Write a function using fat arrow syntax named `arrowMyMap` that accepts an array
 and a callback as arguments. The function will return an array of new elements
-obtained by calling the callback on each element of the array, passing in the 
+obtained by calling the callback on each element of the array, passing in the
 element. Assign the below function to a variable using the const keyword.
 
 Do not use the built in Array.map - use Array.forEach for iteration.
@@ -20,7 +20,20 @@ console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 
 const arrowMyMap = (array, cb) => {
   // Your code here
+  let newArr = [];
+  array.forEach(element => {
+    newArr.push(cb(element));
+  });
+  // console.log(newWord);
+  // newArr.push(newWord);
+  return newArr;
+
 };
+
+const yell = word => word.toUpperCase();
+
+let result2 = arrowMyMap(['run', 'Forrest'], yell);
+console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
