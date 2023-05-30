ee4d4379cf7dea14f01884fe8ef8ef97b5ed732d
Ahkar02468 <arkarshwebaw@gmail.com>
2023-05-30 22:23:28 +0630

    solve 08-09-10

--- problems/08-hidden-counter.js
+++ problems/08-hidden-counter.js
@@ -1,6 +1,6 @@
 /***********************************************************************
 Write a function named `hiddenCounter()`. This function will do two things - first
-it will define a count variable, then it will return a function. 
+it will define a count variable, then it will return a function.
 When invoked the function returned by hiddenCounter will increment the counter by 1.
 
 Look below to see how this function is invoked:
@@ -17,7 +17,16 @@ hidden2(); // returns 1
 
 function hiddenCounter() {
   // Your code here
+  let counter = 1;
+  return function(){
+    return counter++;
+  }
 }
+
+let hidden1 = hiddenCounter(); //returns a function
+console.log(hidden1()); // returns 1
+console.log(hidden1());
+console.log(hidden1());
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
